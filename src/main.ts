ac9b26ef845d52f512b42ed7f9c5caeb535992b2
Josh Koter <josh.koter@gmail.com>
2025-05-24 21:13:16 +0200

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import "./styles.scss";
 
 // setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+export function scrollBtn(element: HTMLButtonElement) {
+  const scrollDown = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth"
+    });
+  };
+
+  element.addEventListener("click", () => scrollDown());
+}
+
+scrollBtn(document.getElementById("scroll-btn") as HTMLButtonElement);
