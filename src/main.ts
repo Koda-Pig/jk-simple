39b118b135eda87c81f9ae60cfe4e60af503bc32
Josh Koter <josh.koter@gmail.com>
2025-05-24 21:24:23 +0200

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import "./styles.scss";
 
 // setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+declare global {
+  interface Window {
+    jk_data: {
+      shouldStopAnimating: boolean;
+    };
+  }
+}
+
+window.jk_data = {
+  shouldStopAnimating: false
+};
+
+const ANIMATION_DURATION = 1000;
 
 export function scrollBtn(element: HTMLButtonElement) {
   const scrollDown = () => {
@@ -9,8 +22,26 @@ export function scrollBtn(element: HTMLButtonElement) {
       behavior: "smooth"
     });
   };
+  const startAnimating = () => {
+    element.classList.add("animate");
+    window.jk_data.shouldStopAnimating = false;
+  };
+  const stopAnimating = () => {
+    window.jk_data.shouldStopAnimating = true;
+  };
+  const animationIteration = () => {
+    if (!window.jk_data.shouldStopAnimating) return;
+    element.classList.remove("animate");
+    window.jk_data.shouldStopAnimating = false;
+  };
 
-  element.addEventListener("click", () => scrollDown());
+  element.addEventListener("animationiteration", animationIteration);
+  element.addEventListener("mouseover", startAnimating);
+  element.addEventListener("mouseleave", stopAnimating);
+  element.addEventListener("focus", startAnimating);
+  element.addEventListener("blur", stopAnimating);
+  element.addEventListener("click", scrollDown);
+  element.style.animationDuration = `${ANIMATION_DURATION}ms`;
 }
 
 scrollBtn(document.getElementById("scroll-btn") as HTMLButtonElement);
