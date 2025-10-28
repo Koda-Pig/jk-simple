7593eabd0f48bca725e406044457fcab89410066
Josh Koter <josh.koter@gmail.com>
2025-10-28 11:22:05 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,16 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "src/partials")
+      partialDirectory: resolve(__dirname, "src/partials"),
+      helpers: {
+        // format the date for the anchor link
+        dateAnchor: (title: string) => {
+          const m = /\((\d{2})\/(\d{2})\/(\d{4})\)/.exec(title || "");
+          if (!m) return "";
+          const [, dd, mm, yyyy] = m;
+          return `${yyyy}-${mm}-${dd}`; // e.g. 2025-10-27
+        }
+      }
     }),
     minifyHTML()
   ],
